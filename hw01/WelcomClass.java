c5ca58d8321fd488eac005408db7273eca7781da
heh219 <heh219@lehigh.edu>
2015-08-31 23:23:26 +0000

    update WelcomClass

--- hw01/WelcomClass.java
+++ hw01/WelcomClass.java
@@ -15,14 +15,14 @@ public class WelcomClass{
   public static void main(String[] args){
       
 //  print the statement with box around it
-    System.out.println("    -------------    ");
-    System.out.println("    |  WELCOME  |    ");
-    System.out.println("    -------------    ");
-    System.out.println("    ^   ^   ^   ^   ");
-    System.out.println("   / \\ / \\ / \\ / \\");
-    System.out.println("<-H--E--H--2--1--9->");
-    System.out.println("   \\ / \\ / \\ / \\ /");
-    System.out.println("    v   v   v   v   ");
+    System.out.println("        -------------    ");
+    System.out.println("        |  WELCOME  |    ");
+    System.out.println("        -------------    ");
+    System.out.println("    ^   ^   ^   ^   ^   ^   ");
+    System.out.println("   / \\ / \\ / \\ / \\ / \\ / \\");
+    System.out.println("<---H---E---H---2---1---9->");
+    System.out.println("   \\ / \\ / \\ / \\ / \\ / \\ /");
+    System.out.println("    v   v   v   v   v   v   ");
     System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
     System.out.println("tour on our tandem. We circled Lake Ontario");
     System.out.println("test");
