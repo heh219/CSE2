2060c8515c480a6c6e769a81bf59cb42c0f2f6e9
heh219 <heh219@lehigh.edu>
2015-08-31 18:02:11 +0000

    updated WelcomClass

---
+++ hw01/WelcomClass.java
@@ -0,0 +1,33 @@
+///////////////////////////////////////////////////////
+//Huang He
+//hw01
+//WelcomClass Program
+//
+//  first compile the program
+//      javac WelcomClass.java
+//  run the program
+//      java WelcomClass//
+
+//  define a class
+public class WelcomClass{
+
+//  add main method
+  public static void main(String[] args){
+      
+//  print the statement with box around it
+    System.out.println("    -------------    ");
+    System.out.println("    |  WELCOME  |    ");
+    System.out.println("    -------------    ");
+    System.out.println("    ^   ^   ^   ^   ");
+    System.out.println("   / \\ / \\ / \\ / \\");
+    System.out.println("<-H--E--H--2--1--9->");
+    System.out.println("   \\ / \\ / \\ / \\ /");
+    System.out.println("    v   v   v   v   ");
+    System.out.println("This August my wife and I took an eleven-day, 675-mile bike");
+    System.out.println("tour on our tandem. We circled Lake Ontario");
+    System.out.println("test");
+    
+    //test/testtest
+    
+  }
+}
