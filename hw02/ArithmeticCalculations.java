78e74b3b51209bd01ffaa342a0bb12d36f872dfb
heh219 <heh219@lehigh.edu>
2015-09-04 15:46:24 +0000

    updated Cyclometer

---
+++ hw02/ArithmeticCalculations.java
@@ -0,0 +1,43 @@
+//////////////////////////////////////
+//Huang He
+//ArithmeticCaculations
+//
+//  first compile the program
+//     javac ArithmeticCaculations.java
+//  run the program
+//     java ArithmeticCaculations//
+//  define a class
+public class ArithmeticCaculations{
+    
+//  add a main method
+  public static void main(String[] args){
+       
+// print the statement with box around it
+//Number of pairs of socks
+int nSocks=3;
+//Cost per pair of socks
+//('$' is part of the variable name)
+double sockCost$=2.58
+
+//Number of drinking glasses
+int nClasses=6;
+//Cost per galss
+double galssCost$=2.29
+
+//Number of box of envelopes
+int nEnvelepes=1;
+//cost per bpx of envelopes
+double envelepeCost$=3.25;
+double taxpercent=0.06;
+
+  System.out.println(double totalSocksCost$=int nSocks*double sockCost$);
+  System.out.println(double totalDringgalssesCost$=int nClasses*double galssCost$);
+  System.out.println(double totalBoxofEnvelopesCost$=int nEnvelepes*double envelepeCost$);
+  System.out.println(double totalpuchasesCost$=double totalSocksCost$+double totalDringgalssesCost$+double totalBoxofEnvelopesCost$);
+  System.out.println(double totalactualpaid$=double totalpuchasesCost$-(double taxpercent*double totalpuchasesCost$));
+
+  //test//testtest
+  
+  }
+
+}
