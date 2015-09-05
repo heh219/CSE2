8cb7db73de50f8aca150bc8141099baa2e57667a
heh219 <heh219@lehigh.edu>
2015-09-05 05:43:39 +0000

    updated ArithmeticCalculations

--- hw02/ArithmeticCalculations.java
+++ hw02/ArithmeticCalculations.java
@@ -3,39 +3,46 @@
 //ArithmeticCaculations
 //
 //  first compile the program
-//     javac ArithmeticCaculations.java
+//     javac ArithmeticCalculations.java
 //  run the program
-//     java ArithmeticCaculations//
+//     java ArithmeticCalculations//
 //  define a class
-public class ArithmeticCaculations{
+public class ArithmeticCalculations{
     
 //  add a main method
   public static void main(String[] args){
        
-// print the statement with box around it
-//Number of pairs of socks
-int nSocks=3;
-//Cost per pair of socks
-//('$' is part of the variable name)
-double sockCost$=2.58
+       //print the statement with box around it
+       //Number of pairs of socks
+         int nSocks=3;//
+       //Cost per pair of socks
+       //('$' is part of the variable name)
+         double sockCost$=2.58;//
+       //Number of drinking glasses
+         int nGlasses=6;//
+       //Cost per galss
+         double galssCost$=2.29;//
+       //Number of box of envelopes
+         int nEnvelopes=1;//
+       //cost per bpx of envelopes
+         double envelopeCost$=3.25;//
+         double taxpercent=0.06;//
+         double totalcostSocks, totalcostGlass, totalcostEnvelope, totalCost;//
+         
 
-//Number of drinking glasses
-int nClasses=6;
-//Cost per galss
-double galssCost$=2.29
-
-//Number of box of envelopes
-int nEnvelepes=1;
-//cost per bpx of envelopes
-double envelepeCost$=3.25;
-double taxpercent=0.06;
-
-  System.out.println(double totalSocksCost$=int nSocks*double sockCost$);
-  System.out.println(double totalDringgalssesCost$=int nClasses*double galssCost$);
-  System.out.println(double totalBoxofEnvelopesCost$=int nEnvelepes*double envelepeCost$);
-  System.out.println(double totalpuchasesCost$=double totalSocksCost$+double totalDringgalssesCost$+double totalBoxofEnvelopesCost$);
-  System.out.println(double totalactualpaid$=double totalpuchasesCost$-(double taxpercent*double totalpuchasesCost$));
-
+         System.out.println("the total cost of socks is "+
+         (nSocks*sockCost$));
+         System.out.println("the total cost of glasses is "+
+         (nGlasses*galssCost$));
+         System.out.println("the total cost of envelopes is "+
+         (nEnvelopes*envelopeCost$));
+         totalcostSocks=nSocks*sockCost$;
+         totalcostEnvelope=nEnvelopes*envelopeCost$;
+         totalcostGlass=nGlasses*galssCost$;
+         totalCost=totalcostGlass+totalcostEnvelope+totalcostSocks;
+         System.out.println("the total cost before tax is "+(totalCost));
+         System.out.println("the actual cost is "+(totalCost*(1+taxpercent)));
+         
   //test//testtest
   
   }
