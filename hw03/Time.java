5845b121c24906a002f8dbec2e705537fab50efd
heh219 <heh219@lehigh.edu>
2015-09-11 20:41:21 +0000

    update Time

---
+++ hw03/Time.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////
+//Huanghe
+//Homework 03
+//Time
+//
+//  first compile this program
+//      javac Time.java
+//  run the program
+//      java Time
+
+import java.util.Scanner;
+//define a class
+public class Time{
+    // main mathod required for every java program
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("enter the curren time in the form HHMM: ");
+        double currentTime = myScanner.nextDouble();
+        System.out.print("enter the time that you will be eating dinner in the form HHMM: ");
+        double dinnerTime = myScanner.nextDouble();
+        int hours, minutes;//to store digits in hours and munites form
+        hours=(int)((dinnerTime-currentTime)/100);//we only need to use HH part
+        minutes=(int)(dinnerTime-currentTime-hours*100);//we only need MM part in munites
+        System.out.println("you have " + hours + " hours and " + minutes + " minutes until dinner");
+    }//end of this method
+}//end of class
