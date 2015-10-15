79b09395abc8e17c97e0e22707af327ab367f54c
heh219 <heh219@lehigh.edu>
2015-10-15 18:03:39 +0000

    update Twisty

--- E.java
+++ E.java
@@ -1,17 +1,18 @@
 public class E{
     public static void main(String[]args){
         for(int m=0;m<4;m++){
-            for(int k=0;k<4,k++){
-                if(k=m){
+            for(int k=0;k<4;k++){
+                if(k==m){
                     System.out.print("#");
                 }
-                else if(k=4-m){
+                else if(k==4-m){
                     System.out.print("/");
                 }
                 else{
                     System.out.print(" ");
                 }
             }
+            System.out.println(" ");
         }
         }
     }
